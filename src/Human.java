0fe8bdad363cb507011bf0873bec74db601655ed
Alex <seredin-alex@mail.ru>
2022-11-28 21:28:06 +0300

    Создан класс Human (30%)

--- src/Human.java
+++ src/Human.java
@@ -1,2 +1,16 @@
 public class Human {
+    int yearOfBirth; // год рождения
+    String name;
+    String town; // город проживания
+    Human(String name, String town, int yearOfBirth) {
+        this.name = name;
+        this.town = town;
+        this.yearOfBirth = yearOfBirth;
+    }
+    void sum() {
+        System.out.println("Привет!");
+        System.out.println("Меня зовут " + name + ". Я из города " + town + ".");
+        System.out.println("Я родился в " + yearOfBirth + " году." + " Будем знакомы!");
+    }
+
 }
--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,11 @@
 public class Main {
     public static void main(String[] args) {
+
         System.out.println("ДЗ-15-ООП");
+        Human maxim = new Human("Максим", "Минск", 1988);
+        maxim.sum();
+
+        Human anna = new Human("Аня", "Москва", 1993);
+        anna.sum();
     }
 }
